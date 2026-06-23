fa5f2991922cf3c2296cfff5e1a8439e1a53ac03
Aman Kumar Patel <aman.nit.bhopal.28@gmail.com>
2026-06-24 03:29:23 +0530

    Create 3967-EarliestFinishTimeForLandAndWaterRidesIi

---
+++ 3967-EarliestFinishTimeForLandAndWaterRidesIi/3967-EarliestFinishTimeForLandAndWaterRidesIi.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/24/2026, 3:29:22 AM
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& startL, vector<int>& durL, vector<int>& startW, vector<int>& durW) {
+        int minL = 1e6, minW = 1e6, res = 1e6;
+        int n = startL.size(), m = startW.size();
+
+        for (int i = 0; i < n; i++)
+            minL = min(minL, startL[i] + durL[i]);
+
+        for (int i = 0; i < m; i++) {
+            minW = min(minW, startW[i] + durW[i]);
+            res = min(res, max(minL, startW[i]) + durW[i]);
+        }
+
+        for (int i = 0; i < n; i++)
+            res = min(res, max(minW, startL[i]) + durL[i]);
+
+        return res;
+    }
+};
