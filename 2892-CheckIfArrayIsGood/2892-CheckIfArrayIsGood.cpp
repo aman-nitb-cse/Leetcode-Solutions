5a59b21d715d8582dbdf306118f594019c79a128
Aman Kumar Patel <aman.nit.bhopal.28@gmail.com>
2026-06-24 03:32:08 +0530

    Create 2892-CheckIfArrayIsGood

---
+++ 2892-CheckIfArrayIsGood/2892-CheckIfArrayIsGood.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/24/2026, 3:32:08 AM
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n = nums.size() - 1;
+        bitset<201> seen;
+        bool dup = 0;
+
+        for (auto& num : nums) {
+            if (num > n) return false;
+
+            if (seen.test(num)) {
+                if (num < n || dup) return false;
+                dup |= 1;
+                continue;
+            }
+
+            seen.set(num);
+        }
+
+        return true;
+    }
+};
