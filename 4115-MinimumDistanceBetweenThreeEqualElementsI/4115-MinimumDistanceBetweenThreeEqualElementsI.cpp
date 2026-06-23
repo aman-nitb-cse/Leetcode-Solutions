f2846bf853cc7ef0290b1644d7ced46de9dbeb3a
Aman Kumar Patel <aman.nit.bhopal.28@gmail.com>
2026-06-24 03:28:48 +0530

    Create 4115-MinimumDistanceBetweenThreeEqualElementsI

---
+++ 4115-MinimumDistanceBetweenThreeEqualElementsI/4115-MinimumDistanceBetweenThreeEqualElementsI.cpp
@@ -0,0 +1,17 @@
+// Last updated: 6/24/2026, 3:28:48 AM
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        int n = nums.size();
+        vector<pair<int,int>> last(n, {-1, -1});
+        int ans = INT_MAX;
+        for(int i = 0; i < n; i++){
+            auto &[x, y] = last[nums[i] - 1];
+            if(x != -1) ans = min(ans, 2 * (i - x));
+
+            x = y;
+            y = i;
+        }
+        return ans < INT_MAX ? ans : -1;
+    }
+};
