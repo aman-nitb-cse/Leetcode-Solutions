e402cac1346ca6932fbcee9209678779692e48c1
Aman Kumar Patel <aman.nit.bhopal.28@gmail.com>
2026-07-03 00:11:59 +0530

    Create 778-SwiminRisingWater

---
+++ 778-SwiminRisingWater/778-SwiminRisingWater.cpp
@@ -0,0 +1,41 @@
+// Last updated: 7/3/2026, 12:11:59 AM
+using ll = long long;
+
+    ll dp[20][2][2];
+
+    ll digitDP(string &s, int i, int tight, int started){
+
+        if(i == s.size()) return 1; // modify
+
+        auto &_dp = dp[i][tight][started];
+
+        if(_dp != -1) return _dp;
+
+        _dp = 0;
+
+        char limit = tight ? s[i] : '9';
+
+        for(char d = '0'; d <= limit; d++){
+
+            _dp += digitDP(
+                s,
+                i + 1,
+                tight && (d == limit),
+                started || d != '0'
+            );
+            
+        }
+
+        return _dp;
+    }
+
+    ll count(ll x){
+
+        if(x < 0) return 0;
+
+        string s = to_string(x);
+
+        memset(dp, -1, sizeof(dp));
+
+        return digitDP(s, 0, 1, 0);
+    }
