e7e529aad0f38a60899ad16d515bd4e8131ff69a
Aman Kumar Patel <aman.nit.bhopal.28@gmail.com>
2026-07-08 01:06:40 +0530

    File updated at 7/8/2026, 1:06:40 AM

--- 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.cpp
+++ 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.cpp
@@ -1,18 +1,14 @@
-// Last updated: 7/8/2026, 1:05:07 AM
+// Last updated: 7/8/2026, 1:06:40 AM
 1class Solution {
 2public:
-3    int pivotInteger(int n) {
-4        int suf = n * (n + 1) / 2;
-5        int pref = 0;
+3    bool isPalindrome(int x) {
+4        string s = to_string(x);
+5
 6
-7        for(int i = 1; i <= n; i++){
-8            pref += i;
-9
-10            if(pref == suf) return i;
-11
-12            suf -= i;
-13        }
-14
-15        return -1;
-16    }
-17};
+7        for(int i = 0; i < s.size() / 2; i++){
+8            if(s[i] != s[s.size() - 1 - i]) return false;
+9        }
+10
+11        return true;
+12    }
+13};
