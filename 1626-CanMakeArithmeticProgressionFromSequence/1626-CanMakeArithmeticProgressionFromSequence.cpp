2bc6d6d54bb0052f7cc6b16458fcf63ff7d33e4f
Aman Kumar Patel <aman.nit.bhopal.28@gmail.com>
2026-08-20 20:51:33 +0530

    Create 1626-CanMakeArithmeticProgressionFromSequence

---
+++ 1626-CanMakeArithmeticProgressionFromSequence/1626-CanMakeArithmeticProgressionFromSequence.cpp
@@ -0,0 +1,15 @@
+// Last updated: 8/20/2026, 8:51:32 PM
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+
+        int d = arr[1] - arr[0];
+
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] - arr[i - 1] != d) return false;
+        }
+
+        return true;
+    }
+};
