47479aa0f9914ee7152685db865ed4eed3c9c12b
Aman Kumar Patel <aman.nit.bhopal.28@gmail.com>
2026-08-20 21:04:30 +0530

    Create 486-PredictTheWinner

---
+++ 486-PredictTheWinner/486-PredictTheWinner.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/20/2026, 9:04:30 PM
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& A) {
+        int n = A.size();
+        if (~n & 1) return true;
+
+        vector<int> dp(n);
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = A[i];
+            for (int j = i + 1; j < n; j++)
+                dp[j] = max(A[i] - dp[j], A[j] - dp[j - 1]);
+        }
+
+        return dp[n - 1] >= 0;
+    }
+};
