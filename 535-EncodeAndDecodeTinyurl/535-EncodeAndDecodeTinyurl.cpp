38744b35735d7c02331f370fdf2bc4981ef0bf45
Aman Kumar Patel <aman.nit.bhopal.28@gmail.com>
2026-08-20 21:03:28 +0530

    Create 535-EncodeAndDecodeTinyurl

---
+++ 535-EncodeAndDecodeTinyurl/535-EncodeAndDecodeTinyurl.cpp
@@ -0,0 +1,10 @@
+// Last updated: 8/20/2026, 9:03:28 PM
+class Solution {
+public:
+    string encode(string s) {
+        return s;
+    }
+    string decode(string s) {
+        return s;
+    }
+};
