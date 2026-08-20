95a7fe5618a40db1af59c28014c6edb5e7e8091a
Aman Kumar Patel <aman.nit.bhopal.28@gmail.com>
2026-08-20 21:17:00 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,14 @@
+// Last updated: 8/20/2026, 9:17:00 PM
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        vector <int> v(n + 1);
+        v[0] = 1;
+        v[1] = 1;
+        for(int i = 2; i <= n; i++){
+            v[i] = v[i - 1] + v[i - 2];
+        }
+        return v[n];
+    }
+};
